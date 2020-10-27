21b0edcd934a4237f9e2b5b4558ec4d4505cf961
Jeongtae Kim <rsatang5@naver.com>
2020-10-27 22:24:29 +0900

    Update global.d.ts

--- src/svelte/global.d.ts
+++ src/svelte/global.d.ts
@@ -2,6 +2,7 @@
 
 interface NodeRequire {
   (id: "electron"): typeof Electron;
+  (id: "electron/common"): typeof Electron.Common;
   (id: "electron/main"): typeof Electron.Main;
   (id: "electron/renderer"): typeof Electron.Renderer;
   (id: "electron-is-dev"): typeof import("electron-is-dev");
